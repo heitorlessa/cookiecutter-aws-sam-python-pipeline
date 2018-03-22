b28d1d36c978dbbc343945e2ff66aee484b288d2
Heitor Lessa <heitor.lessa@hotmail.com>
2018-03-22 10:32:15 +0000

    Adds post hook to allow adding pipeline template and buildspec to current folder to ease implementing pipeline to an existing project

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,65 @@
+"""
+    Given that this cookiecutter will run to create a pipeline
+    to an existing project we will remove the generated folder
+    and copy only what we are interested in.
+"""
+
+from __future__ import print_function
+
+import os
+import shutil
+
+
+TERMINATOR = "\x1b[0m"
+INFO = "\x1b[1;33m [INFO]: "
+SUCCESS = "\x1b[1;32m [SUCCESS]: "
+HINT = "\x1b[3;33m"
+
+
+def copy_files_to_dir(path):
+    filenames = ("buildspec.yaml", "pipeline.yaml")
+    for file in filenames:
+        if os.path.isfile(file):
+            print(
+                INFO +
+                "Copying {} to current directory...".format(file) +
+                TERMINATOR
+            )
+            shutil.copy(file, path)
+
+    return True
+
+
+def remove_gen_project(path):
+    if os.path.exists(path):
+        print(
+            INFO +
+            "Cleaning up leftovers..." +
+            TERMINATOR
+        )
+        shutil.rmtree(path)
+    else:
+        print(
+            HINT +
+            "Folder {} doesn't exist... aborting clean up".format(path) +
+            TERMINATOR
+        )
+
+    return True
+
+
+def main():
+
+    project_slug = '{{ cookiecutter.project_slug }}'
+
+    if copy_files_to_dir('../'):
+        if remove_gen_project('../' + project_slug):
+            print(
+                SUCCESS +
+                "Project initialized successfully!" +
+                TERMINATOR
+            )
+
+
+if __name__ == '__main__':
+    main()
